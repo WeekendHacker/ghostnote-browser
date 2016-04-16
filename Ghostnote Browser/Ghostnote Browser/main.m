cc983ed457b75d101a324c59c86b1159cb580794
Jimmy Hough Jr <=>
2016-04-16 02:06:26 -0500

    Created project, set team.

---
+++ Ghostnote Browser/Ghostnote Browser/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Ghostnote Browser
+//
+//  Created by Jimmy Hough Jr on 4/16/16.
+//  Copyright © 2016 Ghostnnote HQ. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
---
+++ Ghostnote Browser/Ghostnote BrowserTests/Ghostnote_BrowserTests.m
@@ -0,0 +1,39 @@
+//
+//  Ghostnote_BrowserTests.m
+//  Ghostnote BrowserTests
+//
+//  Created by Jimmy Hough Jr on 4/16/16.
+//  Copyright © 2016 Ghostnnote HQ. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ghostnote_BrowserTests : XCTestCase
+
+@end
+
+@implementation Ghostnote_BrowserTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

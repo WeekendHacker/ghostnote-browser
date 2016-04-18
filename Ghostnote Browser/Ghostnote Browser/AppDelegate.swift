f18fb5033834d64eac37333d89d80c894ef4c164
Jimmy Hough Jr <=>
2016-04-18 00:09:09 -0500

    experimental

--- Ghostnote Browser/Ghostnote Browser/AppDelegate.swift
+++ Ghostnote Browser/Ghostnote Browser/AppDelegate.swift
@@ -7,18 +7,26 @@
 //
 
 import Cocoa
+import MagicalRecord
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    @IBOutlet weak var window: NSWindow!
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+            let pathString = "~/Library/Application Support/Ghostnote/GhostNote.sqlite" as NSString
+        
+            let gnURL = NSURL.fileURLWithPath(pathString.stringByExpandingTildeInPath)
+        MagicalRecord.setupCoreDataStackWithAutoMigratingSqliteStoreAtURL(gnURL)
+        
+        let all = GNNote.MR_findAll() as! [GNNote]
         
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-
 }
 
---
+++ Ghostnote Browser/Ghostnote Browser/CoreData/GNNote.swift
@@ -0,0 +1,17 @@
+//
+//  GNNote.swift
+//  Ghostnote Browser
+//
+//  Created by Jimmy Hough Jr on 4/18/16.
+//  Copyright © 2016 Ghostnnote HQ. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(GNNote)
+class GNNote: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}

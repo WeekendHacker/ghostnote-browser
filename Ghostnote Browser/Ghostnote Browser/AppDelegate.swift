b9ff1038f6a7e89fe786d517996248612ff3cd02
Jimmy Hough Jr <=>
2016-04-17 21:50:28 -0500

    Project created

---
+++ Ghostnote Browser/Ghostnote Browser/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Ghostnote Browser
+//
+//  Created by Jimmy Hough Jr on 4/17/16.
+//  Copyright © 2016 Ghostnnote HQ. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Ghostnote Browser/Ghostnote BrowserUITests/Ghostnote_BrowserUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Ghostnote_BrowserUITests.swift
+//  Ghostnote BrowserUITests
+//
+//  Created by Jimmy Hough Jr on 4/17/16.
+//  Copyright © 2016 Ghostnnote HQ. All rights reserved.
+//
+
+import XCTest
+
+class Ghostnote_BrowserUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
